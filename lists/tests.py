d6897555b203dac018bb54ba7d1af1ac7b75d04c
Thiago Mohallem <thiagomohallem1@gmail.com>
2021-02-25 18:34:30 -0300

    Apresentar Múltiplos Itens na Listagem

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,15 @@ from django.test import TestCase
 from lists.models import Item
 class HomePageTest(TestCase):
 
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')  
         self.assertTemplateUsed(response, 'home.html') 
